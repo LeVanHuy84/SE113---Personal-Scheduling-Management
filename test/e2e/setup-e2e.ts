9b9197e9424feb911d261842229bdf80d127e9d7
Tuananh <tuananh48218@gmail.com>
2026-03-31 11:36:30 +0700

    chore(tests): update Jest configuration and setup for end-to-end testing
    - Changed root directory for Jest tests to one level up for better structure.
    - Updated test regex to reflect new directory structure for e2e tests.
    - Modified setup file path for e2e tests to align with the new structure.
    - Added dotenv configuration to load environment variables from a local test file.

--- test/e2e/setup-e2e.ts
+++ test/e2e/setup-e2e.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.test.local' });
 if (!process.env.DATABASE_TEST_URL) {
   throw new Error('Missing DATABASE_TEST_URL for e2e tests');
 }
